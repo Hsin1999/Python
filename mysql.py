5746eafe9d51039f30127adc0951410e5aa0374a
hsin <18525358730@163.com>
2021-09-07 21:33:21 +0800

    mysql

--- mysql.py
+++ mysql.py
@@ -1,4 +1,5 @@
 import pymysql
+#Datebase（地址，用户名，密码，数据库）
 class Datebase():
     def __init__(self,host,user,password,database):
         self.host=host
@@ -13,8 +14,43 @@ class Datebase():
                            cursorclass=pymysql.cursors.DictCursor
                            )
         self.cur=self.db.cursor()
-    def select_Mysql(self,sql):
+    #查询数据（sql语句，查询条数（0：默认全部，1：一条））
+    def select_Mysql(self,sql,a=0):
         self.cur.execute(sql)
+        if isinstance(a,int):
+            if a==1:
+                result=self.cur.fetchone()
+                return result
+            elif a==0:
+                result=self.cur.fetchall()
+                return result
+            else:
+                raise print('查询条数输入错误')
+        else:
+            raise print('参数类型错误（int）')
+    def delete_Mysql(self,sql):
+        try:
+            self.cur.execute(sql)
+            self.db.commit()
+        except:
+            self.db.rollback()
+        self.db.close()
+    def update_Mysql(self,sql):
+        try:
+            self.cur.execute(sql)
+            self.db.commit()
+        except:
+            self.db.rollback()
+        self.db.close()
+    def insert_Mysql(self,sql):
+        try:
+            self.cur.execute(sql)
+            self.db.commit()
+        except:
+            self.db.rollback()
+        self.db.close()
+s=Datebase('47.101.53.77','root','ppaa1122','test')
+print(s.select_Mysql('select * from t_student',1))
 
 
 
@@ -26,11 +62,3 @@ class Datebase():
 
 
 
-
-# db=pymysql.connect(host='47.101.53.77',user='root',password='ppaa1122',database='test')
-# cursor=db.cursor()
-# cursor.execute('insert into t_student values(888,"zzx",2,21,"zhuanye","188888888")')
-# db.commit()
-# cursor.execute('select * from t_student where id=888')
-# result=cursor.fetchone()
-# print(result)

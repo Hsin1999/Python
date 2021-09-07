0b023a21aded35a7323f5ad43bb3de7c4067d8c4
hsin <18525358730@163.com>
2021-09-07 11:14:37 +0800

    del

--- StudentManagementSystem/1.py
+++
@@ -1,8 +0,0 @@
-import hmac
-s=hmac.new(b'hsin', 'data'.encode('utf8'), digestmod='sha256')
-print(s.hexdigest())
-import os
-a=os.path.abspath('.')
-print(a)
-with open(a+r'\files\main.txt', 'r', encoding='utf8') as f:
-    print(f.read())

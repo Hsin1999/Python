2c5d1451359ca9bbcbd7def8ea935bebd49fa089
hsin <18525358730@163.com>
2021-09-08 23:34:23 +0800

    daily

--- 1.py
+++ 1.py
@@ -24,33 +24,33 @@
 # loop = asyncio.get_event_loop()
 # if __name__ =='__main__':
 #     run()
-# def coroutine_example(name):
-#     print('start coroutine...name:', name)
-#     x = yield name #调用next()时，产出yield右边的值后暂停；调用send()时，产出值赋给x，并往下运行
-#     print('send值:', x)
-#     return 'zhihuID: Zarten'
-#
-# def grouper2():
-#     result2 = yield from coroutine_example('Zarten') #在此处暂停，等待子生成器的返回后继续往下执行
-#     print('result2的值：', result2)
-#     return result2
-#
-# def grouper():
-#     result = yield from grouper2() #在此处暂停，等待子生成器的返回后继续往下执行
-#     print('result的值：', result)
-#     return result
-#
-# def main():
-#     g = grouper()
-#     next(g)
-#     print("我继续执行了")
-#     try:
-#         g.send(10)
-#     except StopIteration as e:
-#         print('返回值：', e.value)
-#
-# if __name__ == '__main__':
-#     main()
+def coroutine_example(name):
+    print('start coroutine...name:', name)
+    x = yield name #调用next()时，产出yield右边的值后暂停；调用send()时，产出值赋给x，并往下运行
+    print('send值:', x)
+    return 'zhihuID: Zarten'
+
+def grouper2():
+    result2 = yield from coroutine_example('Zarten') #在此处暂停，等待子生成器的返回后继续往下执行
+    print('result2的值：', result2)
+    return result2
+
+def grouper():
+    result = yield from grouper2() #在此处暂停，等待子生成器的返回后继续往下执行
+    print('result的值：', result)
+    return result
+
+def main():
+    g = grouper()
+    next(g)
+    print("我继续执行了")
+    try:
+        g.send(10)
+    except StopIteration as e:
+        print('返回值：', e.value)
+
+if __name__ == '__main__':
+    main()
 # import asyncio
 # def my_callback(future):
 #     print('返回值：', future.result())
@@ -64,7 +64,7 @@
 # task.add_done_callback(my_callback)
 # loop.run_until_complete(coro)
 # loop.close()
-import asyncio
+# import asyncio
 import threading
 
 # import mysql
@@ -76,15 +76,15 @@ import threading
 #     task=await s.select_Mysql('select * from t_student',1)
 #
 # loop.run_until_complete(select())
-import threading
-import asyncio
-
-async def hello():
-    print('Hello world! (%s)' % threading.currentThread())
-    await asyncio.sleep(1)
-    print('Hello again! (%s)' % threading.currentThread())
-
-loop = asyncio.get_event_loop()
-tasks = [hello(), hello()]
-loop.run_until_complete(asyncio.wait(tasks))
-loop.close()
+# import threading
+# import asyncio
+# async def xc():
+#     print('12321313123')
+# async def hello():
+#     print('Hello world! (%s)' % threading.currentThread())
+#     r=await xc()
+#     await asyncio.sleep(2)
+#     print('Hello again! (%s)' % threading.currentThread())
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(hello())
+# loop.close()

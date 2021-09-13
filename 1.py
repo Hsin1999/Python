71f910c0d10263ffbb8102efb6590695495ad8de
hsin <18525358730@163.com>
2021-09-13 16:37:45 +0800

    异步客户端&服务器

--- 1.py
+++ 1.py
@@ -90,12 +90,56 @@ import threading
 # loop.close()
 import asyncio
 import time
-async def a(x):
-    await asyncio.sleep(x)
-    print('我在运行%s'%x)
-async def main():
-    n=time.time()
-    results=await asyncio.gather(a(1),a(5),a(3),a(4),a(5))
-    s=time.time()-n
-    return s
-print(asyncio.run(main()))
+# async def a(x):
+#     await asyncio.sleep(x)
+#     print('我在运行a%s'%x)
+# async def b(x):
+#     await asyncio.sleep(x)
+#     print('我在运行b%s'%x)
+# async def main():
+#     n=time.time()
+#     task=asyncio.create_task(a(2))
+#     results=await asyncio.gather(a(1),b(5),a(3),a(4),a(5),task)
+#     s=time.time()-n
+#     return s
+# print(asyncio.run(main()))
+import asyncio
+from datetime import datetime
+
+import aiohttp
+from lxml import etree
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit"
+                         "/537.36 (KHTML, like Gecko) "
+                         "Chrome/72.0.3626.121 Safari/537.36"}
+
+
+async def get_movie_url():
+    req_url = "https://movie.douban.com/chart"
+    async with aiohttp.ClientSession(headers=headers) as session:
+        async with session.get(url=req_url, headers=headers) as response:
+            result = await response.text()
+            result = etree.HTML(result)
+            print(result.xpath("//*[@id='content']/div/div[1]/div/div/table/tr/td/a/@href"))
+        return result.xpath("//*[@id='content']/div/div[1]/div/div/table/tr/td/a/@href")
+
+
+async def get_movie_content(movie_url):
+    async with aiohttp.ClientSession(headers=headers) as session:
+        async with session.get(url=movie_url, headers=headers) as response:
+            result = await response.text()
+            result = etree.HTML(result)
+        movie = dict()
+        name = result.xpath('//*[@id="content"]/h1/span[1]//text()')
+        author = result.xpath('//*[@id="info"]/span[1]/span[2]//text()')
+        movie["name"] = name
+        movie["author"] = author
+    return movie
+
+if __name__ == '__main__':
+    start = datetime.now()
+    loop = asyncio.get_event_loop()
+    movie_url_list = loop.run_until_complete(get_movie_url())
+    tasks = [get_movie_content(url) for url in movie_url_list]
+    movies = loop.run_until_complete(asyncio.gather(*tasks))
+    print(movies)
+    print("异步用时为：{}".format(datetime.now() - start))
--- web.py
+++ web.py
@@ -4,12 +4,14 @@ import logging
 logging.basicConfig(level=logging.INFO)
 from aiohttp import web
 routes = web.RouteTableDef()
+async def get_text():
+    with open('1.txt','r',encoding='utf8') as f:
+        return f.read()
 @routes.get('/')
 async def index(request):
-    s = mysql.Database('47.101.53.77', 'root', 'ppaa1122', 'test')
-    a=s.select_Mysql('select * from t_student',0)
-    aa=str(a).encode('utf8')
-    await asyncio.sleep(0.5)
+    # s = mysql.Database('47.101.53.77', 'root', 'ppaa1122', 'test')
+    # a=s.select_Mysql('select * from t_student',0)
+    aa=await get_text()
     return web.Response(body=aa,content_type='text/html',charset='utf8')
 @routes.get('/{name}')
 async def hello(request):

2fe731528808b8045a7112d4d73f756bc7e1a2a1
hsin <18525358730@163.com>
2021-10-21 10:40:51 +0800

    update

--- cat douban.py
+++ cat douban.py
@@ -1,17 +1,14 @@
 import requests,re,os
 from bs4 import BeautifulSoup
-response=requests.request('get','https://movie.douban.com/',headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'})
+header={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'}
+response=requests.request('get','https://movie.douban.com/',headers=header)
 bs=BeautifulSoup(response.content,'html.parser')
 bss=bs.body.find_all("img",src=re.compile("img"))
-# print(bss)
-rea=re.compile(r'src="(.*?)"')
+rea=re.compile(r'src="(.*?)"'
 a=[]
 for i in bss:
     a.append(rea.findall(str(i))[0])
-# print(a)
-# print(os.path.realpath(__file__))
 path=os.path.realpath(__file__)
-# print(os.path.join(os.path.dirname(path),'photo'))
 s=0
 for i in a:
     s+=1
